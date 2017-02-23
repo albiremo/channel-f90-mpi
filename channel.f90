ca33486493504455117d88ead171715212e9b353
Davide Gatti <davide.gatti@kit.edu>
2017-02-23 22:54:42 +0900

    corrected typo

--- channel.f90
+++ channel.f90
@@ -47,7 +47,7 @@ PROGRAM channel
   ALLOCATE(localpsd(-1:ny+1,-nz:nz,2:5),globalpsd(-1:ny+1,-nz:nz,2:5))
   IF (has_terminal) THEN 
     INQUIRE(FILE="stats.dat",EXIST=io)
-    IF (io) THEN 
+    IF (.NOT. io) THEN 
       istats=0; globalstats=0; localpsd=0; globalpsd=0
       OPEN(UNIT=102,FILE='stats.dat',STATUS="new",ACCESS='stream',ACTION='readwrite')
     ELSE
